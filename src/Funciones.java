dbb19f86fe59e51514e28f2a6503d71790b51f03
VictorQuinoa <vquinoamunoz@danielcastelao.org>
2023-11-17 09:35:13 +0100

    programa

--- src/Funciones.java
+++ src/Funciones.java
@@ -1,44 +1,50 @@
 import java.util.Scanner;
 
 /**
- * Funciones utilizadas en la clase main
- * @author dam1
- * @version v1.0
+ * Funciones utilizadas en la clase Main
  */
 public class Funciones {
     /**
-     * ccmprueba si el sueldo esta entre 1000 y 1750
+     * Comprueba si el sueldo está entre 1000 y 1750
+     *
      * @param sueldo del trabajador
-     * @return si esta dentro del rango
+     * @return si está dentro del rango
      */
-    public static Boolean obtenerSueldo1750(Double sueldo){
-        if(sueldo>=1000 && sueldo<=1750){
-           return true;
-        }
-        return false;
-    }
-    public static Boolean obtenerSueldo1000(Double sueldo) {
-        if (sueldo < 1000) {
+    public static Boolean obtenerSueldo1750(Double sueldo) {
+        if (sueldo > 1000 && sueldo < 1750) {
             return true;
         }
         return false;
     }
 
-    public static boolean obtenerSueldoMas1750(Double sueldo){
-        if (sueldo>1750){
+    /**
+     * Comprueba si el sueldo es menor que mil
+     *
+     * @param sueldo del trabajador
+     * @return si es menor que mil
+     */
+    public static Boolean obtenerSueldo1000(Double sueldo) {
+        if (sueldo > 0 && sueldo < 1000) {
             return true;
         }
         return false;
     }
+
+    /**
+     * Alta trabajador
+     *
+     * @return trabajador creado
+     */
     public static Trabajador darAltaTrabajador() {
         Trabajador obxTrabajador = new Trabajador();
         //obxTrabajador.setNombre("Pepe");
         //obxTrabajador.setSueldo(1200d);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nombre?");
+        obxTrabajador.setNombre(sc.nextLine());
+        System.out.println("Sueldo?");
+        obxTrabajador.setSueldo(sc.nextDouble());
 
+        return obxTrabajador;
     }
-    Scanner sc =new Scanner(System.in);
-    System.out.println("Nombre?");
-    obxTrabajador.setNombre(sc.nextLine());
-    System.out.println("Sueldo?");
-    obxTrabajador.setSueldo(sc.nextDouble());
 }
